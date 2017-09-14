f61a16f37e4ee6c09842c6922366d4d4e30ec1ed
Connor <czoop@me.com>
2017-09-14 14:50:33 -0700

    Initial Commit

---
+++ src/MathMagic.java
@@ -0,0 +1,52 @@
+public class MathMagic {
+	private int num1;
+	private int num2;
+	
+	public MathMagic(int num1, int num2) {
+		this.num1 = num1;
+		this.num2 = num2;
+	}
+	
+	public MathMagic() {
+		this.num1 = 1;
+		this.num2 = 2;
+	}
+	
+	//////////////////////////
+	
+	public String artihmetic() {
+		String mathInfo = "";
+		mathInfo = num1 + " + " + num2 + " = " + (num1 + num2) + "\n" ;
+		mathInfo = num1 + " - " + num2 + " = " + (num1 - num2) + "\n" ;
+		mathInfo = num2 + " - " + num1 + " = " + (num2 - num1) + "\n" ;
+		mathInfo = num1 + " x " + num2 + " = " + (num1 * num2) + "\n" ;
+		mathInfo = num1 + " / " + num2 + " = " + (num1 / num2) + "\n" ;
+		mathInfo = num2 + " / " + num1 + " = " + (num2 / num1) + "\n" ;
+		mathInfo = num1 + " % " + num2 + " = " + (num1 % num2) + "\n" ;
+		mathInfo = num2 + " % " + num1 + " = " + (num2 % num1) + "\n" ;
+		
+		return mathInfo;
+	}
+	
+	public int 
+	
+	//////////////////////////
+	
+	public int getNum1() {
+		return this.num1;
+	}
+	
+	public int getNum2() {
+		return this.num2;
+	}
+	
+	//////////////////////////
+	
+	public void setNum1(int num1) {
+		this.num1 = num1;
+	}
+	
+	public void setNum2(int num2) {
+		this.num2 = num2;
+	}
+}
---
+++ src/MathMagicRunner.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class MathMagicRunner {
+	public static void main(String[] arg) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the first Int: ");
+		int number1 = input.nextInt();
+		System.out.println("Enter the second Int: ");
+		int number2 = input.nextInt();
+		
+		MathMagic mathMagic = new MathMagic(number1, number 2);
+	}
+}
